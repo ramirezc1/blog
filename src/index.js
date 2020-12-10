5fb91d61ebcca5890a00a83ec76e776f910485c8
Cristian Ramirez <ramirezcristian81@live.com>
2020-12-10 13:04:09 -0800

    cleaned up project and added styled-components and react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+const theme = {
+  textColor: "black",
+  linkColor: "white",
+};
 
+const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+}
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'avenir next', avenir, 'helvetica neue', helvetica, ubuntu, roboto, noto, 'segoe ui', arial, sans-serif;
+    color: black;
+    background-color: white;
+    font-size: calc(1em + 1vw);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    ${"" /* width: 100vw;
+    height: 100vh;
+    user-select: none; */}
+  } 
+`;
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
